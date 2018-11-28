0b19c4e816a28f9b7573a4547b21018d770a3c8c
Spencer Parkin <stparkin@mmm.com>
2018-11-28 10:18:12 -0700

    first attempt to get something interesting...completely failiure!...must be something wrong with the math

--- utah_ava_map.js
+++ utah_ava_map.js
@@ -178,37 +178,117 @@ AvalancheHazardImageryProvider.prototype._generate_tile_canvas = function(rose, 
     
     let context = canvas.getContext('2d');
     
-    // TODO: We should only shade terrain that is in a certain slope-angle range (38 degrees +/- 10 degrees.)
+    let row_step = 16;
+    let col_step = 16;
     
-    let di = 16;
-    let dj = 16;
+    let height_field_rows = canvas.height / row_step;
+    let height_field_cols = canvas.width / col_step;
     
-    let debug_height = 0.0;
-    
-    for(let i = 0; i < canvas.width; i += di) {
-        for(let j = 0; j < canvas.height; j += dj) {
-            
-            let longitude = tileRect.west + (i + di/2) / canvas.width * (tileRect.east - tileRect.west);
-            let latitude = tileRect.south + (canvas.height - 1 - j + dj/2) / canvas.height * (tileRect.north - tileRect.south);
+    // Generate a height field for the tile.  This is the most expensive part.
+    let height_field = [];
+    for(let i = 0; i < height_field_rows; i++) {
+        let column = [];
+        height_field.push(column);
+        let longitude = tileRect.west + ((i + 0.5) / height_field_rows) * (tileRect.east - tileRect.west);
+        for(let j = 0; j < height_field_cols; j++) {
+            let latitude = tileRect.south + (1.0 - (j + 0.5) / height_field_cols) * (tileRect.north - tileRect.south);
             let cartographic = new Cesium.Cartographic(longitude, latitude);
-            let height = viewer.scene.globe.getHeight(cartographic);
-            
-            if(i == 128 && j == 128)
-                debug_height = height;
-            
-            let r = i % 256;
-            let g = j % 256;
-            let b = (i + j) % 256;
-            let a = 255;
-            context.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
-            context.fillRect(i, j, di, dj);
+            cartographic.height = viewer.scene.globe.getHeight(cartographic); // Slow ray cast!
+            let position = Cesium.Cartographic.toCartesian(cartographic);
+            column.push({position: position, aspect: undefined, slope_angle: undefined});
         }
     }
-
-    context.font = 'bold 25px Arial';
-    context.textAlign = 'center';
-    context.fillStyle = 'white';
-    context.fillText(debug_height.toFixed(1), 127, 127);
+    
+    // Now go calculate aspect and slope-angle for each point in the height field.
+    for(let i = 0; i < height_field_rows; i++) {
+        for(let j = 0; j < height_field_cols; j++) {
+            let entry = height_field[i][j];
+            
+            // Collect and compute normals from adjacent entries in the height field.
+            let normal;
+            let vector_a = new Cesium.Cartesian3();
+            let vector_b = new Cesium.Cartesian3();
+            let normal_list = [];
+            if(i + 1 < height_field_rows) {
+                if(j + 1 < height_field_cols) {
+                    Cesium.Cartesian3.subtract(height_field[i+1][j].position, entry.position, vector_a);
+                    Cesium.Cartesian3.subtract(height_field[i][j+1].position, entry.position, vector_b);
+                    normal = new Cesium.Cartesian3();
+                    Cesium.Cartesian3.cross(vector_a, vector_b, normal)
+                    Cesium.Cartesian3.normalize(normal, normal);
+                    normal_list.push(normal);
+                }
+                if(j > 0) {
+                    Cesium.Cartesian3.subtract(height_field[i][j-1].position, entry.position, vector_a);
+                    Cesium.Cartesian3.subtract(height_field[i+1][j].position, entry.position, vector_b);
+                    normal = new Cesium.Cartesian3();
+                    Cesium.Cartesian3.cross(vector_a, vector_b, normal)
+                    Cesium.Cartesian3.normalize(normal, normal);
+                    normal_list.push(normal);
+                }
+            }
+            if(i > 0) {
+                if(j + 1 < height_field_cols) {
+                    Cesium.Cartesian3.subtract(height_field[i][j+1].position, entry.position, vector_a);
+                    Cesium.Cartesian3.subtract(height_field[i-1][j].position, entry.position, vector_b);
+                    normal = new Cesium.Cartesian3();
+                    Cesium.Cartesian3.cross(vector_a, vector_b, normal)
+                    Cesium.Cartesian3.normalize(normal, normal);
+                    normal_list.push(normal);
+                }
+                if(j > 0) {
+                    Cesium.Cartesian3.subtract(height_field[i-1][j].position, entry.position, vector_a);
+                    Cesium.Cartesian3.subtract(height_field[i][j-1].position, entry.position, vector_b);
+                    normal = new Cesium.Cartesian3();
+                    Cesium.Cartesian3.cross(vector_a, vector_b, normal)
+                    Cesium.Cartesian3.normalize(normal, normal);
+                    normal_list.push(normal);
+                }
+            }
+            
+            // Average the normals to get our best approximation.
+            normal = new Cesium.Cartesian3(0.0, 0.0, 0.0);
+            for(let k = 0; k < normal_list.length; k++)
+                Cesium.Cartesian3.add(normal, normal_list[k], normal);
+            Cesium.Cartesian3.normalize(normal, normal);
+            
+            // Finally, calculate aspect and slope-angle.
+            entry.aspect = Math.atan2(normal.z, normal.x);
+            let normal_projected = new Cesium.Cartesian3(normal.x, 0.0, normal.z);
+            let angle = Cesium.Cartesian3.angleBetween(normal, normal_projected);
+            entry.slope_angle = angle * 180.0 / Math.PI;
+        }
+    }
+    
+    let prime_slope_angle = 38.0;
+    let prime_slope_angle_radius = 5.0;
+    let slope_angle_min = prime_slope_angle - prime_slope_angle_radius;
+    let slope_angle_max = prime_slope_angle + prime_slope_angle_radius;
+    
+    let r, g, b, a;
+    
+    for(let i = 0; i < canvas.height; i += row_step) {
+        for(let j = 0; j < canvas.width; j += col_step) {
+            let entry = height_field[i / row_step][j / col_step];
+            if(entry.slope_angle >= slope_angle_min && entry.slope_angle <= slope_angle_max) {
+                
+                // TODO: Color the avalanchy slope based on its aspect/elevation and the avalanche rose.
+            
+                r = 255;
+                g = 255;
+                b = 255;
+                a = 255;
+            } else {
+                r = 0;
+                g = 0;
+                b = 0;
+                a = 0;
+            }
+            
+            context.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
+            context.fillRect(j, i, col_step, row_step);
+        }
+    }
 
     return canvas;
 };
