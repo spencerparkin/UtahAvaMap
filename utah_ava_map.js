a28aa0116f1bf097cd098ebb2d934ddce47dadf7
Spencer Parkin <stparkin@mmm.com>
2018-12-12 13:49:11 -0700

    this and the ElevationRamp material types are the key to what i'm trying to do, but i'm having trouble figuring out where these or any built-in materials are implimented

--- utah_ava_map.js
+++ utah_ava_map.js
@@ -10,18 +10,50 @@ var init_map = function() {
         })
     });
     
-    let west = -111.7287239132627;
+    viewer.scene.globe.enableLighting = true;
+    
+    /*let west = -111.7287239132627;
     let east = -111.6729336993894;
     let north = 40.66047397914876;
     let south = 40.64897595231015;
     let rect = Cesium.Rectangle.fromDegrees(west, south, east, north);
-    viewer.camera.setView({destination: rect});
+    viewer.camera.setView({destination: rect});*/
 }
 
 window.onload = function() {
     init_map();
 }
 
+function getColorRamp() {
+    var ramp = document.createElement('canvas');
+    ramp.width = 100;
+    ramp.height = 1;
+    var ctx = ramp.getContext('2d');
+    var values = [0.0, 0.29, 0.5, Math.sqrt(2)/2, 0.87, 0.91, 1.0];
+    var grd = ctx.createLinearGradient(0, 0, 100, 0);
+    grd.addColorStop(values[0], '#000000'); //black
+    grd.addColorStop(values[1], '#2747E0'); //blue
+    grd.addColorStop(values[2], '#D33B7D'); //pink
+    grd.addColorStop(values[3], '#D33038'); //red
+    grd.addColorStop(values[4], '#FF9742'); //orange
+    grd.addColorStop(values[5], '#ffd700'); //yellow
+    grd.addColorStop(values[6], '#ffffff'); //white
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, 100, 1);
+    return ramp;
+}
+
 var debug_click = function() {
-    console.log('Debug!');
+    /*var material = new Cesium.Material({
+        fabric: {
+            type: 'Color',
+            uniforms: {
+                color: new Cesium.Color(1.0, 0.0, 0.0, 0.5)
+            }
+        }
+    });*/
+    var material = Cesium.Material.fromType('SlopeRamp');
+    material.uniforms.image = getColorRamp();
+    
+    viewer.scene.globe.material = material;
 }
