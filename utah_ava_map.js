dd102712ed0bfd5a64d9756814bab9f641bfb114
Spencer Parkin <stparkin@mmm.com>
2018-12-20 12:36:18 -0700

    start of view-model/knockout stuff

--- utah_ava_map.js
+++ utah_ava_map.js
@@ -1,6 +1,10 @@
 // utah_ava_map.js
 
 var viewer = null;
+var viewModel = {
+    slope_prime_radius: 15.0
+};
+var ava_material = null;
 
 var init_map = function() {
     Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZjBmNGUxMi1mNjYyLTQ4NTMtYjdkZC03ZGJkMzZlMzYyZWQiLCJpZCI6NTA2Miwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0MjMwODg2MH0.MJB-IG9INCNEA0ydUvprHcUTLdKDbnPpkWG6DCqXKQc';
@@ -18,9 +22,37 @@ var init_map = function() {
     let south = 40.64897595231015;
     let rect = Cesium.Rectangle.fromDegrees(west, south, east, north);
     viewer.camera.setView({destination: rect});
+    
+    promiseAvaMapShader().then(glsl_code => {
+        ava_material = new Cesium.Material({
+            fabric: {
+                type: 'UtahAvaMaterial',
+                source: glsl_code,
+                uniforms: {
+                    // Default to extreme danger everywhere.  We'll update this as a function of the camera position/orientation.
+                    rose00: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
+                    rose01: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
+                    rose10: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
+                    rose11: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
+                    rose20: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
+                    rose21: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
+                    // Look at slopes in the range [P-15,P+15], where P is the prime avalanche slope angle (38 degrees.)
+                    slope_prime_radius: viewModel.slope_prime_radius
+                }
+            }
+        });
+        
+        viewer.scene.globe.material = ava_material;
+    });
 }
 
 window.onload = function() {
+    Cesium.knockout.track(viewModel);
+    Cesium.knockout.applyBindings(viewModel, document.getElementById('cesiumControls'));
+    Cesium.knockout.getObservable(viewModel, 'slope_prime_radius').subscribe(() => {
+        ava_material.uniforms.slope_prime_radius = viewModel.slope_prime_radius;
+    });
+    
     init_map();
 }
 
@@ -41,25 +73,5 @@ function promiseAvaMapShader() {
 }
 
 var debug_click = function() {
-    promiseAvaMapShader().then(glsl_code => {
-        var material = new Cesium.Material({
-            fabric: {
-                type: 'UtahAvaMaterial',
-                source: glsl_code,
-                uniforms: {
-                    // Default to extreme danger everywhere.  We'll change this once the real danger rating comes in.
-                    rose00: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
-                    rose01: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
-                    rose10: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
-                    rose11: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
-                    rose20: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
-                    rose21: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
-                    // Look at slopes in the range [P-15,P+15], where P is the prime avalanche slope angle (38 degrees.)
-                    slope_prime_radius: 15.0
-                }
-            }
-        });
-        
-        viewer.scene.globe.material = material;
-    });
+    
 }
