da04a8c12ba163f909d56b53aa67307dfe2c1b91
SpencerParkin <spencerparkin@outlook.com>
2018-12-21 22:33:10 -0700

    identify nearest ava region as camera moves around

--- utah_ava_map.js
+++ utah_ava_map.js
@@ -3,9 +3,11 @@
 var viewer = null;
 var viewModel = {
     slope_prime_radius: 15.0,
-    slope_prime_alpha: 0.7
+    slope_prime_alpha: 0.7,
+    ava_region: ''
 };
 var ava_material = null;
+var ava_regions_map = null;
 
 var init_map = function() {
     Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZjBmNGUxMi1mNjYyLTQ4NTMtYjdkZC03ZGJkMzZlMzYyZWQiLCJpZCI6NTA2Miwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0MjMwODg2MH0.MJB-IG9INCNEA0ydUvprHcUTLdKDbnPpkWG6DCqXKQc';
@@ -37,7 +39,7 @@ var init_map = function() {
                     rose11: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
                     rose20: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
                     rose21: new Cesium.Color(1.0, 1.0, 1.0, 1.0),
-                    // Look at slopes in the range [P-15,P+15], where P is the prime avalanche slope angle (38 degrees.)
+                    // Look at slopes in the range [P-R,P+R], where P is the prime avalanche slope angle (38 degrees.)
                     slope_prime_radius: viewModel.slope_prime_radius,
                     slope_prime_alpha: viewModel.slope_prime_alpha
                 }
@@ -46,9 +48,7 @@ var init_map = function() {
         
         viewer.scene.globe.material = ava_material;
     });
-}
 
-window.onload = function() {
     Cesium.knockout.track(viewModel);
     Cesium.knockout.applyBindings(viewModel, document.getElementById('cesiumControls'));
     Cesium.knockout.getObservable(viewModel, 'slope_prime_radius').subscribe(() => {
@@ -57,8 +57,105 @@ window.onload = function() {
     Cesium.knockout.getObservable(viewModel, 'slope_prime_alpha').subscribe(() => {
         ava_material.uniforms.slope_prime_alpha = viewModel.slope_prime_alpha;
     });
-    
+    Cesium.knockout.getObservable(viewModel, 'ava_region').subscribe(() => {
+        /*promiseAvaRose(viewModel.ava_region).then(ava_rose => {
+            ava_material.uniforms.rose00 = new Cesium.Color();
+            ava_material.uniforms.rose01 = new Cesium.Color();
+            ava_material.uniforms.rose10 = new Cesium.Color();
+            ava_material.uniforms.rose11 = new Cesium.Color();
+            ava_material.uniforms.rose20 = new Cesium.Color();
+            ava_material.uniforms.rose21 = new Cesium.Color();
+        });*/
+    });
+
+    viewer.camera.changed.addEventListener(event => {
+        updateNearestAvaRegion();
+    });
+
+    promiseAvaRegions().then(json_data => {
+        ava_regions_map = json_data;
+        updateNearestAvaRegion();
+    });
+}
+
+window.onload = function() {
     init_map();
+}
+
+function updateNearestAvaRegion() {
+    let center = {x: viewer.canvas.width / 2.0, y: viewer.canvas.height / 2.0};
+    let cartographic = screenCoordsToCartographic(center);
+    if(Cesium.defined(cartographic)) {
+        let ava_region = findNearestAvaRegion(cartographic);
+        if(viewModel.ava_region !== ava_region) {
+            viewModel.ava_region = ava_region;
+        }
+    }
+}
+
+function findNearestAvaRegion(cartographic) {
+    let nearest_region;
+    let smallest_distance = 99999999.0;
+    for(let region in ava_regions_map) {
+        let region_cartographic = ava_regions_map[region];
+        let distance = cartographicDistance(cartographic, region_cartographic);
+        if(distance < smallest_distance) {
+            smallest_distance = distance;
+            nearest_region = region;
+        }
+    }
+    return nearest_region;
+}
+
+function cartographicDistance(cartA, cartB) {
+    return Math.sqrt(
+        (cartA.longitude - cartB.longitude) * (cartA.longitude - cartB.longitude) +
+        (cartA.latitude - cartB.latitude) * (cartA.latitude - cartB.latitude) +
+        (cartA.height - cartB.height) * (cartA.height - cartB.height)
+    );
+}
+
+function screenCoordsToCartographic(screen_point) {
+    let ray = viewer.camera.getPickRay(screen_point);
+    if(Cesium.defined(ray) && !isNaN(ray.direction.x)) {
+        let ground_point = viewer.scene.globe.pick(ray, viewer.scene);
+        if(Cesium.defined(ground_point)) {
+            return Cesium.Ellipsoid.WGS84.cartesianToCartographic(ground_point);
+        }
+    }
+    return undefined;
+}
+
+function promiseAvaRose() {
+}
+
+function promiseAvaRegions() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: 'ava_regions',
+            dataType: 'json',
+            success: json_data => {
+                if('error' in json_data) {
+                    alert(json_data['error']);
+                    reject();
+                } else {
+                    for(let region in json_data) {
+                        let location = json_data[region];
+                        json_data[region] = new Cesium.Cartographic(
+                            location.longitude * (Math.PI / 180.0),
+                            location.latitude * (Math.PI / 180.0),
+                            location.altitude
+                        );
+                    }
+                    resolve(json_data);
+                }
+            },
+            failure: error => {
+                alert(error);
+                reject();
+            }
+        });
+    });
 }
 
 function promiseAvaMapShader() {
@@ -75,8 +172,4 @@ function promiseAvaMapShader() {
             }
         });
     });
-}
-
-var debug_click = function() {
-    
 }
--- utah_ava_map.py
+++ utah_ava_map.py
@@ -19,6 +19,34 @@ class WebServer(object):
     def default(self, **kwargs):
         return cherrypy.lib.static.serve_file(self.root_dir + '/utah_ava_map.html', content_type='text/html')
 
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def ava_regions(self, **kwargs):
+        try:
+            print('Loading avalanche region data...')
+            ava_regions_map = {}
+            namespaces = {'kml': 'http://www.opengis.net/kml/2.2'}
+            import xml.etree.ElementTree as ET
+            tree = ET.parse('AvalancheRegions.kml')
+            root = tree.getroot()
+            doc = root.find('kml:Document', namespaces)
+            for folder in doc.findall('kml:Folder', namespaces):
+                if folder.find('kml:name', namespaces).text == 'AvalancheRegions':
+                    for placemark in folder.findall('kml:Placemark', namespaces):
+                        name = placemark.find('kml:name', namespaces).text
+                        print('Found: %s' % name)
+                        coordinates_text = placemark.find('kml:Point', namespaces).find('kml:coordinates', namespaces).text
+                        coordinates_list = coordinates_text.split(',')
+                        location = {
+                            'longitude': float(coordinates_list[0]),
+                            'latitude': float(coordinates_list[1]),
+                            'altitude': float(coordinates_list[2])
+                        }
+                        ava_regions_map[name] = location
+            return ava_regions_map
+        except Exception as ex:
+            return {'error': str(ex)}
+
 if __name__ == '__main__':
     root_dir = os.path.dirname(os.path.abspath(__file__))
     port = int(os.environ.get('PORT', 5200))
